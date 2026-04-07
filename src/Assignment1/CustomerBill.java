333496522f902471554ba871f4029b407243f2bf
Shahar Ankonina <shahar.ankonina05@gmail.com>
2026-04-07 12:56:17 -0700

    Completed Assignment 1

--- src/Assignment1/CustomerBill.java
+++ src/Assignment1/CustomerBill.java
@@ -1,10 +1,70 @@
 package Assignment1;
 
-public class CustomerBill {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class CustomerBill
+{
+	public static void main(String[] args)
+	{
+		final float TV_PRICE = 400.00f;
+		final float VCR_PRICE = 220.00f;
+		final float REMOTE_CONTROLLER_PRICE = 35.20f;
+		final float CD_PRICE = 300.00f;
+		final float TAPE_RECORDER_PRICE = 150.00f;
+		final float TAX_RATE = 0.0825f;
+		
+		int TVQuantity = 0;
+		int VCRQuantity = 0;
+		int remoteQuantity = 0;
+		int CDQuantity = 0;
+		int recorderQuantity = 0;
+		
+		float TVTotal = 0f;
+		float VCRTotal = 0f;
+		float remoteTotal = 0f;
+		float CDTotal = 0f;
+		float recorderTotal = 0f;
+		
+		float subtotal = 0f;
+		float tax = 0f;
+		float total = 0f;
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("How many TV's were sold?");
+		TVQuantity = in.nextInt();
+		TVTotal = TVQuantity * TV_PRICE;
+		
+		System.out.println("How many VCR's were sold?");
+		VCRQuantity = in.nextInt();
+		VCRTotal = VCRQuantity * VCR_PRICE;
+		
+		System.out.println("How many remote controller's were sold?");
+		remoteQuantity = in.nextInt();
+		remoteTotal = remoteQuantity * REMOTE_CONTROLLER_PRICE;
+		
+		System.out.println("How many CD's were sold?");
+		CDQuantity = in.nextInt();
+		CDTotal = CDQuantity * CD_PRICE;
+		
+		System.out.println("How many Tape Recorder's were sold?");
+		recorderQuantity = in.nextInt();
+		recorderTotal = recorderQuantity * TAPE_RECORDER_PRICE;
+		
+		subtotal = TVTotal + VCRTotal + remoteTotal + CDTotal + recorderTotal;
+		tax = subtotal * TAX_RATE;
+		total = subtotal + tax;
+		
+		System.out.printf("QTY\t\tDESCRIPTION\t\tUNIT PRICE\t\tTOTAL PRICE\n");
+		System.out.printf("%d\t\tTV\t\t\t$%.2f\t\t\t$%.2f\n", TVQuantity, TV_PRICE, TVTotal);
+		System.out.printf("%d\t\tVCR\t\t\t$%.2f\t\t\t$%.2f\n", VCRQuantity, VCR_PRICE, VCRTotal);
+		System.out.printf("%d\t\tRemote Controller\t$%.2f\t\t\t$%.2f\n", remoteQuantity, REMOTE_CONTROLLER_PRICE, remoteTotal);
+		System.out.printf("%d\t\tCD\t\t\t$%.2f\t\t\t$%.2f\n", CDQuantity, CD_PRICE, CDTotal);
+		System.out.printf("%d\t\tTape Recorder\t\t$%.2f\t\t\t$%.2f\n", recorderQuantity, TAPE_RECORDER_PRICE, recorderTotal);
+		System.out.printf("\nSubtotal:\t$%.2f\n", subtotal);
+		System.out.printf("Tax:\t\t$%.2f\n", tax);
+		System.out.printf("Total:\t\t$%.2f\n", total);
+		
+		in.close();
 	}
-
 }
--- src/Assignment1/TemperatureConversion.java
+++ src/Assignment1/TemperatureConversion.java
@@ -1,10 +1,24 @@
 package Assignment1;
 
+import java.util.Scanner;
+
 public class TemperatureConversion {
+    public static void main(String[] args) {
+        float fahrenheit, centigrade;
+        Scanner in = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.println("Enter a temperature in Centigrade:");
+        centigrade = in.nextFloat();
+        fahrenheit = 32 + (centigrade * 180.0f / 100.0f);
 
-	}
+        System.out.printf("%.2f degrees Centigrade is equal to %.2f degrees Fahrenheit.\n", centigrade, fahrenheit);
+        
+        System.out.println("Enter a temperature in Fahrenheit:");
+        fahrenheit = in.nextFloat();
+        centigrade = (fahrenheit - 32) * 100.0f / 180.0f;
 
+        System.out.printf("%.2f degrees Fahrenheit is equal to %.2f degrees Centigrade.\n", fahrenheit, centigrade);
+
+        in.close();
+    }
 }
